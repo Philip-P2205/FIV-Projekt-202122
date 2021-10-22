257e90c7158d2bd0cf249d653da8709c936de378
Philip-P2205 <pipip2205@gmail.com>
2021-10-22 20:53:14 +0200

    added dark mode support in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from './services/dark-mode.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'FIV-Project';
+
+  /**
+   * @param darkModeService dark mode state is stored in localStorage
+   */
+  constructor(private darkModeService: DarkModeService) {}
+
+  get darkMode(): boolean {
+    return this.darkModeService.isDarkModeEnabled();
+  }
 }
