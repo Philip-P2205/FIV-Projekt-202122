c2beb14fb1af039102b7b708007bb903a9f3a4f9
Philip-P2205 <pipip2205@gmail.com>
2021-10-23 13:01:37 +0200

    added error color definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,31 @@ module.exports = {
     },
     darkMode: 'class', // or 'media' or 'false'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    dark: {
+                        bg: '#282C34',
+                        txt: '#F5F6Fa'
+                    }
+                },
+                secondary: {
+                    dark: {
+                        bg: '#21252B',
+                        btn: '#404754',
+                        hover: '#4D5565',
+                        txt: '#CCCCCC'
+                    }
+                },
+                tertiary: {
+                    dark: {
+                        bg: '#2C313B',
+                        txt: '#9A9B9E'
+                    }
+                },
+                error: '#EB2F06'
+            }
+        },
     },
     variants: {
         extend: {},
